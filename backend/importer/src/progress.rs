380bcd48d1b6eb2bd978413539c85d5f295f96eb
Jens Reimann <ctron@dentrassi.de>
2024-03-08 13:10:56 +0100

    feat(import): add a progress meter for getting an ETA

---
+++ backend/importer/src/progress.rs
@@ -0,0 +1,26 @@
+use indicatif::MultiProgress;
+use indicatif_log_bridge::LogWrapper;
+use std::io::IsTerminal;
+use walker_common::progress::indicatif::MultiIndicatif;
+use walker_common::progress::{NoProgress, Progress};
+
+/// Set up the env_logger and attach a progress interface if we are running on a terminal.
+pub(crate) fn init_log_and_progress() -> Progress {
+    let mut builder = env_logger::builder();
+    let logger = builder.build();
+
+    match std::io::stdin().is_terminal() {
+        true => {
+            let max_level = logger.filter();
+            let multi = MultiProgress::new();
+
+            let log = LogWrapper::new(multi.clone(), logger);
+            // NOTE: LogWrapper::try_init is buggy and messes up the log levels
+            log::set_boxed_logger(Box::new(log)).unwrap();
+            log::set_max_level(max_level);
+
+            Progress::new(MultiIndicatif(multi))
+        }
+        false => Progress::new(NoProgress),
+    }
+}
