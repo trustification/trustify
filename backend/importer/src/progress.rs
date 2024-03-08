9ff55a8e76478e2f4425ace7c070c3390b6c2806
Jens Reimann <ctron@dentrassi.de>
2024-03-08 17:41:50 +0100

    chore: remove unwrap()

--- backend/importer/src/progress.rs
+++ backend/importer/src/progress.rs
@@ -1,10 +1,11 @@
+use anyhow::Context;
 use indicatif::MultiProgress;
 use indicatif_log_bridge::LogWrapper;
 use std::io::IsTerminal;
 use walker_common::progress::Progress;
 
 /// Set up the env_logger and attach a progress interface if we are running on a terminal.
-pub(crate) fn init_log_and_progress() -> Progress {
+pub(crate) fn init_log_and_progress() -> anyhow::Result<Progress> {
     let mut builder = env_logger::builder();
     let logger = builder.build();
 
@@ -15,11 +16,11 @@ pub(crate) fn init_log_and_progress() -> Progress {
 
             let log = LogWrapper::new(multi.clone(), logger);
             // NOTE: LogWrapper::try_init is buggy and messes up the log levels
-            log::set_boxed_logger(Box::new(log)).unwrap();
+            log::set_boxed_logger(Box::new(log)).context("failed to initialize logger")?;
             log::set_max_level(max_level);
 
-            multi.into()
+            Ok(multi.into())
         }
-        false => Progress::default(),
+        false => Ok(Progress::default()),
     }
 }
