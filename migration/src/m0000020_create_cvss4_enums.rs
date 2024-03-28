dc92063b6189d2ab94eca49b31d16ea60da4bdb5
Gilles Dubreuil <gdubreui@redhat.com>
2024-03-28 11:58:59 +0100

    Cleanup

--- migration/src/m0000020_create_cvss4_enums.rs
+++ migration/src/m0000020_create_cvss4_enums.rs
@@ -145,6 +145,7 @@ impl MigrationTrait for Migration {
         manager
             .drop_type(Type::drop().name(Cvss4At::Cvss4At).to_owned())
             .await?;
+
         manager
             .drop_type(Type::drop().name(Cvss4Ac::Cvss4Ac).to_owned())
             .await?;
