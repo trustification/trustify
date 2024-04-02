32d61f8cb3f9a6037b45a672adb134c69070f3d5
Jim Crossley <jim@crossleys.org>
2024-04-02 16:38:07 -0400

    refactor: loader delegation in Format::load now
    Fixes #125
    
    Signed-off-by: Jim Crossley <jim@crossleys.org>

--- modules/ingestor/src/service/advisory/mod.rs
+++ modules/ingestor/src/service/advisory/mod.rs
@@ -1,5 +1,8 @@
+use crate::service::advisory::{csaf::loader::CsafLoader, osv::loader::OsvLoader};
 use crate::service::Error;
+use std::io::Read;
 use std::str::FromStr;
+use trustify_module_graph::graph::Graph;
 
 pub mod csaf;
 pub mod osv;
@@ -17,6 +20,27 @@ impl FromStr for Format {
             "osv" => Ok(Self::OSV),
             "csaf" => Ok(Self::CSAF),
             _ => Err(Error::UnsupportedFormat(s.into())),
+        }
+    }
+}
+
+impl<'g> Format {
+    pub async fn load<R: Read>(
+        &self,
+        graph: &'g Graph,
+        source: &str,
+        reader: R,
+        checksum: &str,
+    ) -> Result<String, Error> {
+        match self {
+            Format::CSAF => {
+                let loader = CsafLoader::new(graph);
+                loader.load(source, reader, checksum).await
+            }
+            Format::OSV => {
+                let loader = OsvLoader::new(graph);
+                loader.load(source, reader, checksum).await
+            }
         }
     }
 }
