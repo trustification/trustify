de4b0b526ef15d1a619c7f4b6939c66c3be12132
Jens Reimann <ctron@dentrassi.de>
2024-04-17 16:23:36 +0200

    chore: update "large" test

--- modules/graph/src/graph/sbom/spdx/tests/perf.rs
+++ modules/graph/src/graph/sbom/spdx/tests/perf.rs
@@ -78,17 +78,16 @@ async fn ingest_spdx_medium() -> Result<(), anyhow::Error> {
 // ignore because it's a slow slow slow test.
 #[ignore]
 #[test(tokio::test)]
-async fn parse_spdx_openshift() -> Result<(), anyhow::Error> {
-    let db = Database::for_test("parse_spdx_openshift").await?;
+async fn ingest_spdx_large() -> Result<(), anyhow::Error> {
+    let db = Database::for_test("ingest_spdx_large").await?;
     let system = Graph::new(db);
 
-    let sbom = open_sbom("openshift-4.13.json")?;
-
-    let start = Instant::now();
+    let sbom = open_sbom_xz("openshift-4.13.json.xz")?;
 
     let tx = system.transaction().await?;
-    let (spdx, _) = parse_spdx(sbom)?;
 
+    let start = Instant::now();
+    let (spdx, _) = parse_spdx(sbom)?;
     let parse_time = start.elapsed();
 
     let start = Instant::now();
@@ -101,35 +100,31 @@ async fn parse_spdx_openshift() -> Result<(), anyhow::Error> {
             Transactional::None,
         )
         .await?;
+    let ingest_time_1 = start.elapsed();
 
+    let start = Instant::now();
     sbom.ingest_spdx(spdx, &tx).await?;
-    tx.commit().await?;
+    let ingest_time_2 = start.elapsed();
 
-    let ingest_time = start.elapsed();
+    let start = Instant::now();
+    tx.commit().await?;
+    let commit_time = start.elapsed();
+
     let start = Instant::now();
 
     let described_cpe222 = sbom.describes_cpe22s(Transactional::None).await?;
-    assert_eq!(1, described_cpe222.len());
+    log::info!("{:#?}", described_cpe222);
+    assert_eq!(3, described_cpe222.len());
 
     let described_packages = sbom.describes_packages(Transactional::None).await?;
     log::info!("{:#?}", described_packages);
 
-    let contains = sbom
-        .related_packages(
-            Relationship::ContainedBy,
-            described_packages[0].clone().into(),
-            Transactional::None,
-        )
-        .await?;
-
-    println!("{}", contains.len());
-
-    assert!(contains.len() > 500);
-
     let query_time = start.elapsed();
 
     log::info!("parse: {}", humantime::Duration::from(parse_time));
-    log::info!("ingest: {}", humantime::Duration::from(ingest_time));
+    log::info!("ingest 1: {}", humantime::Duration::from(ingest_time_1));
+    log::info!("ingest 2: {}", humantime::Duration::from(ingest_time_2));
+    log::info!("commit: {}", humantime::Duration::from(commit_time));
     log::info!("query: {}", humantime::Duration::from(query_time));
 
     Ok(())
