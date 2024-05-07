5abdb6172fc08d0c9835ae9fc8ac2dd589789350
Bob McWhirter <bmcwhirt@redhat.com>
2024-05-07 15:01:14 -0400

    Fix for #237 Adjust relationships between vulnerability and advisory join table. Add tests for each endpoint.

--- entity/src/advisory.rs
+++ entity/src/advisory.rs
@@ -21,12 +21,11 @@ pub struct Model {
 
 #[derive(Copy, Clone, Debug, EnumIter, DeriveRelation)]
 pub enum Relation {
-    #[sea_orm(has_many = "super::advisory_vulnerability::Entity")]
-    AdvisoryVulnerabilities,
+    //#[sea_orm(has_many = "super::advisory_vulnerability::Entity")]
+    //AdvisoryVulnerabilities,
 
-    #[sea_orm(has_many = "super::vulnerability::Entity")]
-    Vulnerability,
-
+    //#[sea_orm(has_many = "super::vulnerability::Entity")]
+    //Vulnerability,
     #[sea_orm(has_many = "super::cvss3::Entity")]
     Cvss3,
 
@@ -40,11 +39,14 @@ pub enum Relation {
     NotAffectedPackageVersion,
 }
 
+/*
 impl Related<advisory_vulnerability::Entity> for Entity {
     fn to() -> RelationDef {
         Relation::AdvisoryVulnerabilities.def()
     }
 }
+
+ */
 
 impl Related<vulnerability::Entity> for Entity {
     fn to() -> RelationDef {
