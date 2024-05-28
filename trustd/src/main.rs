d58927be899cc57a9db193d08142eaee7d6bfca3
Helio Frota <00hf11@gmail.com>
2024-05-28 07:42:09 -0300

    chore: extracts sample_data

--- trustd/src/main.rs
+++ trustd/src/main.rs
@@ -1,16 +1,11 @@
 use clap::Parser;
+use sample_data::sample_data;
 use std::env;
 use std::process::{ExitCode, Termination};
-use std::time::Duration;
 use tokio::task::{spawn_local, LocalSet};
-use trustify_common::config::Database;
-use trustify_module_importer::model::{
-    CommonImporter, CsafImporter, ImporterConfiguration, SbomImporter,
-};
-use trustify_module_importer::service::{Error, ImporterService};
-use url::Url;
 
 mod db;
+mod sample_data;
 
 #[allow(clippy::large_enum_variant)]
 #[derive(clap::Subcommand, Debug)]
@@ -71,54 +66,6 @@ async fn pm_mode() -> anyhow::Result<ExitCode> {
     LocalSet::new()
         .run_until(async { spawn_local(api.run()).await? })
         .await
-}
-
-async fn sample_data(db: &Database) -> anyhow::Result<()> {
-    let db = trustify_common::db::Database::new(db).await?;
-
-    let importer = ImporterService::new(db);
-    importer
-        .create(
-            "redhat-sbom".into(),
-            ImporterConfiguration::Sbom(SbomImporter {
-                common: CommonImporter {
-                    disabled: true,
-                    period: Duration::from_secs(300),
-                    description: Some("All Red Hat SBOMs".into())
-                },
-                source: "https://access.redhat.com/security/data/sbom/beta/".to_string(),
-                keys: vec![
-                    Url::parse("https://access.redhat.com/security/data/97f5eac4.txt#77E79ABE93673533ED09EBE2DCE3823597F5EAC4")?
-                ],
-                v3_signatures: true,
-                only_patterns: vec![],
-            }),
-        )
-        .await.or_else(|err|match err {
-            Error::AlreadyExists(_) =>Ok(()),
-            err => Err(err)
-        })?;
-    importer
-        .create(
-            "redhat-csaf-vex-2024".into(),
-            ImporterConfiguration::Csaf(CsafImporter {
-                common: CommonImporter {
-                    disabled: true,
-                    period: Duration::from_secs(300),
-                    description: Some("Red Hat VEX files from 2024".into()),
-                },
-                source: "redhat.com".to_string(),
-                v3_signatures: true,
-                only_patterns: vec!["^cve-2024-".into()],
-            }),
-        )
-        .await
-        .or_else(|err| match err {
-            Error::AlreadyExists(_) => Ok(()),
-            err => Err(err),
-        })?;
-
-    Ok(())
 }
 
 #[tokio::main]
