0f9edfe4475de5f62cf8c239f8fa2f61e3a413b2
Jens Reimann <ctron@dentrassi.de>
2024-05-31 15:33:02 +0200

    ci: fix redundant messages when adding multiple reviewers

--- .github/scripts/map-users/test.mjs
+++ .github/scripts/map-users/test.mjs
@@ -1,12 +1,14 @@
 import * as fs from 'fs';
 import { channelMessage, Mapper } from "./main.mjs";
 
-const github = JSON.parse(fs.readFileSync('payload.json'))?.github;
+// const github = JSON.parse(fs.readFileSync('payload.json'))?.github;
+// const output = channelMessage(github.event)
 
-const output = channelMessage(github.event)
+const payload = JSON.parse(fs.readFileSync('payload.json'));
+const output = channelMessage(payload)
 
 console.log("Output:", output)
 
-const args = new Mapper(github.event).directMessageArguments();
+const args = new Mapper(payload).directMessageArguments();
 console.log("Direct:", args)
 
