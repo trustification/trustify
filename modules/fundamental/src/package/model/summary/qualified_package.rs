6ed4321e3209b82ab03c5d42c2577e11d10a16b3
Bob McWhirter <bmcwhirt@redhat.com>
2024-06-06 15:21:50 -0400

    Provide searching for base versionless and fully-qualified packages using `?q=`. Add some endpoint tests demonstrating using a more complex filter..

--- modules/fundamental/src/package/model/summary/qualified_package.rs
+++ modules/fundamental/src/package/model/summary/qualified_package.rs
@@ -1,11 +1,13 @@
 use crate::package::model::{PackageHead, PackageVersionHead, QualifiedPackageHead};
 use crate::Error;
-use sea_orm::LoaderTrait;
+use sea_orm::{LoaderTrait, ModelTrait};
 use serde::{Deserialize, Serialize};
 use trustify_common::db::ConnectionOrTransaction;
+use trustify_common::paginated;
 use trustify_entity::{package, package_version, qualified_package};
+use utoipa::ToSchema;
 
-#[derive(Serialize, Deserialize, Debug)]
+#[derive(Serialize, Deserialize, Clone, Debug, ToSchema)]
 pub struct QualifiedPackageSummary {
     #[serde(flatten)]
     pub head: QualifiedPackageHead,
@@ -22,32 +24,36 @@ impl QualifiedPackageSummary {
             .load_one(package_version::Entity, tx)
             .await?;
 
-        let packages = qualified_packages.load_one(package::Entity, tx).await?;
-
         let mut summaries = Vec::new();
 
-        for ((package, package_version), qualified_package) in packages
-            .iter()
-            .zip(package_versions.iter())
-            .zip(qualified_packages.iter())
+        for (package_version, qualified_package) in
+            package_versions.iter().zip(qualified_packages.iter())
         {
-            if let (Some(package), Some(package_version), qualified_package) =
-                (package, package_version, qualified_package)
+            if let (Some(package_version), qualified_package) = (package_version, qualified_package)
             {
-                summaries.push(QualifiedPackageSummary {
-                    head: QualifiedPackageHead::from_entity(
-                        package,
-                        package_version,
-                        qualified_package,
-                        tx,
-                    )
-                    .await?,
-                    base: PackageHead::from_entity(package, tx).await?,
-                    version: PackageVersionHead::from_entity(package, package_version, tx).await?,
-                })
+                if let Some(package) = package_version
+                    .find_related(package::Entity)
+                    .one(tx)
+                    .await?
+                {
+                    summaries.push(QualifiedPackageSummary {
+                        head: QualifiedPackageHead::from_entity(
+                            &package,
+                            package_version,
+                            qualified_package,
+                            tx,
+                        )
+                        .await?,
+                        base: PackageHead::from_entity(&package, tx).await?,
+                        version: PackageVersionHead::from_entity(&package, package_version, tx)
+                            .await?,
+                    })
+                }
             }
         }
 
         Ok(summaries)
     }
 }
+
+paginated!(QualifiedPackageSummary);
