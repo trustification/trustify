dea876a7f0d838ea121a4e112156bd7de29ea5ee
Helio Frota <00hf11@gmail.com>
2024-06-18 11:55:50 -0300

    test: ensuring the embedded UI works

--- modules/ui/src/endpoints.rs
+++ modules/ui/src/endpoints.rs
@@ -5,3 +5,33 @@ use trustify_ui::{trustify_ui, UI};
 pub fn configure(config: &mut web::ServiceConfig, ui: &UI) {
     config.service(ResourceFiles::new("/", trustify_ui(ui)).resolve_not_found_to(""));
 }
+
+#[cfg(test)]
+mod test {
+    use super::*;
+    use actix_web::{test, App};
+
+    #[test]
+    async fn test_ui_get() {
+        let test_ui = UI {
+            version: "".to_string(),
+            auth_required: "".to_string(),
+            oidc_server_url: "".to_string(),
+            oidc_client_id: "".to_string(),
+            oidc_scope: "".to_string(),
+            analytics_enabled: "".to_string(),
+            analytics_write_key: "".to_string(),
+        };
+
+        let app = test::init_service(App::new().configure(|cfg| configure(cfg, &test_ui))).await;
+
+        let req = test::TestRequest::get().uri("/").to_request();
+        let resp = test::call_service(&app, req).await;
+        assert!(resp.status().is_success());
+
+        let body = test::read_body(resp).await;
+        let body_str = std::str::from_utf8(&body).unwrap();
+        assert!(body_str.contains("Trustification"));
+        assert!(body_str.contains("You need to enable JavaScript to run this app"));
+    }
+}
