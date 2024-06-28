bef4c9d99c529018d8409c2221782682bd16fe51
Jens Reimann <ctron@dentrassi.de>
2024-06-28 15:38:00 +0200

    chore: update UI module and adapt to new API

--- modules/ui/src/endpoints.rs
+++ modules/ui/src/endpoints.rs
@@ -1,7 +1,37 @@
 use actix_web::web;
-use actix_web_static_files::ResourceFiles;
+use actix_web_static_files::{deps::static_files::Resource, ResourceFiles};
+use std::collections::HashMap;
 use trustify_ui::{trustify_ui, UI};
 
-pub fn configure(config: &mut web::ServiceConfig, ui: &UI) {
-    config.service(ResourceFiles::new("/", trustify_ui(ui)).resolve_not_found_to(""));
+pub struct UiResources {
+    resources: HashMap<&'static str, Resource>,
+}
+
+impl UiResources {
+    pub fn new(ui: &UI) -> anyhow::Result<Self> {
+        Ok(Self {
+            resources: trustify_ui(ui)?,
+        })
+    }
+
+    pub fn resources(&self) -> HashMap<&'static str, Resource> {
+        self.resources
+            .iter()
+            .map(|(k, v)| {
+                // unfortunately, we can't just clone, but we can do it ourselves
+                (
+                    *k,
+                    Resource {
+                        data: v.data,
+                        modified: v.modified,
+                        mime_type: v.mime_type,
+                    },
+                )
+            })
+            .collect()
+    }
+}
+
+pub fn configure(config: &mut web::ServiceConfig, ui: &UiResources) {
+    config.service(ResourceFiles::new("/", ui.resources()).resolve_not_found_to(""));
 }
