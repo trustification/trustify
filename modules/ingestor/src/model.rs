b941d7e7b6c9e92f6e4c01c52d53d84132862124
Jens Reimann <ctron@dentrassi.de>
2024-07-10 09:36:51 +0200

    docs: describe IngestResult struct and fields

--- modules/ingestor/src/model.rs
+++ modules/ingestor/src/model.rs
@@ -1,9 +1,13 @@
 use trustify_common::id::Id;
 
+/// The result of the ingestion process
 #[derive(Clone, Debug, serde::Deserialize, serde::Serialize)]
 pub struct IngestResult {
+    /// The internal ID of the document
     pub id: Id,
+    /// The ID declared by the document
     pub document_id: String,
+    /// Warnings that occurred during the import process
     #[serde(default, skip_serializing_if = "Vec::is_empty")]
     pub warnings: Vec<String>,
 }
