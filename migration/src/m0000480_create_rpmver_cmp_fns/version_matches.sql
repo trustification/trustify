878f2bcd668a97f254b6306fadf4b21ff7a70e31
Bob McWhirter <bmcwhirt@redhat.com>
2024-07-23 14:35:25 -0400

    Add a few more semver-sorting case/when's to version-matching.

--- migration/src/m0000480_create_rpmver_cmp_fns/version_matches.sql
+++ migration/src/m0000480_create_rpmver_cmp_fns/version_matches.sql
@@ -6,15 +6,29 @@ declare
 begin
     return case
         when range_p.version_scheme_id = 'semver'
+            -- Semver is semver
+            then semver_version_matches(version_p, range_p)
+        when range_p.version_scheme_id = 'gem'
+            -- RubyGems claims to be semver
+            then semver_version_matches(version_p, range_p)
+        when range_p.version_scheme_id = 'npm'
+            -- NPM claims to be semver
+            then semver_version_matches(version_p, range_p)
+        when range_p.version_scheme_id = 'golang'
+            -- Golang claims to be semver
+            then semver_version_matches(version_p, range_p)
+        when range_p.version_scheme_id = 'nuget'
+            -- NuGet claims to be semver
+            then semver_version_matches(version_p, range_p)
+        when range_p.version_scheme_id = 'generic'
+            -- Might was well try semver
             then semver_version_matches(version_p, range_p)
         when range_p.version_scheme_id = 'rpm'
+            -- Look at me! I'm an RPM! I'm special!
             then rpmver_version_matches(version_p, range_p)
-        when range_p.version_scheme_id = 'generic'
-            then semver_version_matches(version_p, range_p)
         else
             false
     end;
-
 end
 $$
     language 'plpgsql';
