10880deb1cdf508926d3b3b61ac6c3b7e2e60203
Bob McWhirter <bmcwhirt@redhat.com>
2024-07-23 13:17:16 -0400

    Add unit test and fix bad copy-pasta for rpmver_cmp.

--- migration/src/m0000480_create_rpmver_cmp_fns/rpmver_cmp.sql
+++ migration/src/m0000480_create_rpmver_cmp_fns/rpmver_cmp.sql
@@ -2,23 +2,23 @@
 create or replace function rpmver_cmp(a text, b text)
     returns integer as $$
 declare
-    a_segments varchar[];
-    b_segments varchar[];
+    a_segments text[];
+    b_segments text[];
     a_len integer;
     b_len integer;
-    a_seg varchar;
-    b_seg varchar;
+    a_seg text;
+    b_seg text;
 begin
     if a = b then return 0; end if;
-    a_segments := array(select (regexp_matches(a, '(\\d+|[a-za-z]+|[~^])', 'g'))[1]);
-    b_segments := array(select (regexp_matches(b, '(\\d+|[a-za-z]+|[~^])', 'g'))[1]);
+    a_segments := array(select (regexp_matches(a, '(\d+|[a-zA-Z]+|[~^])', 'g'))[1]);
+    b_segments := array(select (regexp_matches(b, '(\d+|[a-zA-Z]+|[~^])', 'g'))[1]);
     a_len := array_length(a_segments, 1);
     b_len := array_length(b_segments, 1);
     for i in 1..coalesce(least(a_len, b_len) + 1, 0) loop
         a_seg = a_segments[i];
         b_seg = b_segments[i];
-        if a_seg ~ '^\\d' then
-            if b_seg ~ '^\\d' then
+        if a_seg ~ '^\d' then
+            if b_seg ~ '^\d' then
                 a_seg := ltrim(a_seg, '0');
                 b_seg := ltrim(b_seg, '0');
                 case
@@ -29,7 +29,7 @@ begin
             else
                 return 1;
             end if;
-        elsif b_seg ~ '^\\d' then
+        elsif b_seg ~ '^\d' then
             return -1;
         elsif a_seg = '~' then
             if b_seg != '~' then
@@ -45,7 +45,11 @@ begin
             return -1;
         end if;
         if a_seg != b_seg then
-            if a_seg < b_seg then return -1; else return 1; end if;
+            if a_seg < b_seg then
+                return -1;
+            else
+                return 1;
+            end if;
         end if;
     end loop;
     if b_segments[a_len + 1] = '~' then return 1; end if;
---
+++ migration/tests/version_common.rs
@@ -0,0 +1,65 @@
+use migration::ConnectionTrait;
+use migration::sea_orm::Statement;
+use trustify_common::db::Database;
+
+pub enum VersionRange {
+    Exact(&'static str),
+    Range(Version, Version),
+}
+
+pub enum Version {
+    Inclusive(&'static str),
+    Exclusive(&'static str),
+    Unbounded,
+}
+
+pub async fn version_matches(
+    db: &Database,
+    candidate: &str,
+    range: VersionRange,
+) -> Result<bool, anyhow::Error> {
+    let (low, low_inclusive, high, high_inclusive) = match range {
+        VersionRange::Exact(version) => (
+            Some(version.to_string()),
+            true,
+            Some(version.to_string()),
+            true,
+        ),
+        VersionRange::Range(low, high) => {
+            let (low, low_inclusive) = match low {
+                Version::Inclusive(version) => (Some(version.to_string()), true),
+                Version::Exclusive(version) => (Some(version.to_string()), false),
+                Version::Unbounded => (None, false),
+            };
+
+            let (high, high_inclusive) = match high {
+                Version::Inclusive(version) => (Some(version.to_string()), true),
+                Version::Exclusive(version) => (Some(version.to_string()), false),
+                Version::Unbounded => (None, false),
+            };
+
+            (low, low_inclusive, high, high_inclusive)
+        }
+    };
+
+    let low = low.map(|v| format!("'{v}'")).unwrap_or("null".to_string());
+    let high = high.map(|v| format!("'{v}'")).unwrap_or("null".to_string());
+
+    if let Some(result) = db
+        .query_one(Statement::from_string(
+            db.get_database_backend(),
+            format!(
+                r#"
+        SELECT * FROM version_matches('{candidate}',
+            (null, 'semver', {low}, {low_inclusive}, {high}, {high_inclusive})::version_range
+        );
+                "#,
+            ),
+        ))
+        .await?
+    {
+        Ok(result.try_get_by_index::<bool>(0)?)
+    } else {
+        Ok(false)
+    }
+}
