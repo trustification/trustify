f9727a21e8b7d91ff6d5d53d41bbcfbb47003f61
Jim Crossley <jim@crossleys.org>
2024-08-08 12:33:46 -0400

    feat: refactor temp file creation to reduce S3 usage
    Now both the FS and S3 backends share the same logic to create a temp
    file before permanently storing its content keyed by its digest. This
    enables a single S3 write per document rather than the previous
    potentially expensive write/copy/delete.
    
    Signed-off-by: Jim Crossley <jim@crossleys.org>

---
+++ modules/storage/src/service/temp.rs
@@ -0,0 +1,62 @@
+use bytes::Bytes;
+use futures::{Stream, StreamExt};
+use std::{
+    fmt::Debug,
+    io::{Error, SeekFrom},
+    pin::pin,
+};
+use tempfile::tempfile;
+use tokio::{
+    fs::File,
+    io::{AsyncRead, AsyncSeekExt, AsyncWriteExt},
+};
+use trustify_common::hashing::{Contexts, Digests};
+
+use super::StorageResult;
+
+pub struct TempFile {
+    file: File,
+    digests: Digests,
+}
+
+/// Writes the contents of a stream to a temporary file and provides a
+/// unique key for consumers to use to write the contents elsewhere,
+/// e.g. Filesystem or S3.
+impl TempFile {
+    pub async fn new<S, E>(stream: S) -> Result<Self, Error>
+    where
+        E: Debug,
+        S: Stream<Item = Result<Bytes, E>>,
+    {
+        let mut file = File::from(tempfile()?);
+        let mut stream = pin!(stream);
+        let mut contexts = Contexts::new();
+
+        while let Some(next) = stream
+            .next()
+            .await
+            .transpose()
+            .map_err(|e| Error::other(format!("{e:?}")))?
+        {
+            contexts.update(&next);
+            file.write_all(&next).await?;
+        }
+        let digests = contexts.finish();
+
+        Ok(Self { file, digests })
+    }
+
+    /// Return a clone of the temp file after resetting its position
+    pub async fn reader(&mut self) -> Result<impl AsyncRead, Error> {
+        self.file.seek(SeekFrom::Start(0)).await?;
+        self.file.try_clone().await
+    }
+
+    /// Passing self should ensure self.file is dropped and hence
+    /// deleted from the filesystem
+    pub fn result(self) -> StorageResult {
+        StorageResult {
+            digests: self.digests,
+        }
+    }
+}
