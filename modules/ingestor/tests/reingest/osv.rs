ae0472f752408de8858d5a6b9c2726a48a1425e0
Jens Reimann <ctron@dentrassi.de>
2024-09-09 15:11:11 +0200

    fix: allow ingesting YAML based OSV
    This not only requires processing of YAML files, but also detecting them
    with the Format enum.

--- modules/ingestor/tests/reingest/osv.rs
+++ modules/ingestor/tests/reingest/osv.rs
@@ -1,62 +1,135 @@
 #![allow(clippy::expect_used)]
 
 use anyhow::bail;
+use std::future::Future;
 use test_context::test_context;
 use test_log::test;
 use trustify_common::id::Id;
 use trustify_module_ingestor::model::IngestResult;
+use trustify_module_ingestor::service::Format;
 use trustify_test_context::TrustifyContext;
 
 #[test_context(TrustifyContext)]
 #[test(tokio::test)]
-async fn reingest(ctx: &TrustifyContext) -> Result<(), anyhow::Error> {
-    async fn assert(ctx: &TrustifyContext, result: IngestResult) -> anyhow::Result<()> {
-        let Id::Uuid(id) = result.id else {
-            bail!("must be an id")
-        };
-        let adv = ctx
-            .graph
-            .get_advisory_by_id(id, ())
-            .await?
-            .expect("must be found");
+async fn reingest_json(ctx: &TrustifyContext) -> Result<(), anyhow::Error> {
+    reingest(
+        ctx,
+        "osv/RUSTSEC-2021-0079.json",
+        Format::OSV,
+        |ctx, result| async move {
+            assert_common(ctx, &result, "CVE-2021-32714").await?;
+            Ok(())
+        },
+    )
+    .await
+}
 
-        assert_eq!(adv.vulnerabilities(()).await?.len(), 1);
+#[test_context(TrustifyContext)]
+#[test(tokio::test)]
+async fn reingest_json_unknown(ctx: &TrustifyContext) -> Result<(), anyhow::Error> {
+    reingest(
+        ctx,
+        "osv/RUSTSEC-2021-0079.json",
+        Format::Unknown,
+        |ctx, result| async move {
+            assert_common(ctx, &result, "CVE-2021-32714").await?;
+            Ok(())
+        },
+    )
+    .await
+}
 
-        let all = adv.vulnerabilities(&()).await?;
-        assert_eq!(all.len(), 1);
-        assert_eq!(
-            all[0].advisory_vulnerability.vulnerability_id,
-            "CVE-2021-32714"
-        );
+#[test_context(TrustifyContext)]
+#[test(tokio::test)]
+async fn reingest_yaml(ctx: &TrustifyContext) -> Result<(), anyhow::Error> {
+    reingest(
+        ctx,
+        "osv/RSEC-2023-6.yaml",
+        Format::OSV,
+        |ctx, result| async move {
+            assert_common(ctx, &result, "CVE-2020-5238").await?;
+            Ok(())
+        },
+    )
+    .await
+}
 
-        let all = ctx.graph.get_vulnerabilities(()).await?;
-        assert_eq!(all.len(), 1);
+#[test_context(TrustifyContext)]
+#[test(tokio::test)]
+async fn reingest_yaml_unknown(ctx: &TrustifyContext) -> Result<(), anyhow::Error> {
+    reingest(
+        ctx,
+        "osv/RSEC-2023-6.yaml",
+        Format::Unknown,
+        |ctx, result| async move {
+            assert_common(ctx, &result, "CVE-2020-5238").await?;
+            Ok(())
+        },
+    )
+    .await
+}
 
-        let vuln = ctx
-            .graph
-            .get_vulnerability("CVE-2021-32714", ())
-            .await?
-            .expect("Must be found");
-
-        assert_eq!(vuln.vulnerability.id, "CVE-2021-32714");
-
-        let descriptions = vuln.descriptions("en", ()).await?;
-        assert_eq!(descriptions.len(), 0);
-
-        Ok(())
-    }
-
+async fn reingest<'a, F, Fut>(
+    ctx: &'a TrustifyContext,
+    file: &'static str,
+    format: Format,
+    assert: F,
+) -> Result<(), anyhow::Error>
+where
+    F: Fn(&'a TrustifyContext, IngestResult) -> Fut + 'a,
+    Fut: Future<Output = anyhow::Result<()>> + 'a,
+{
     // ingest once
 
-    let result = ctx.ingest_document("osv/RUSTSEC-2021-0079.json").await?;
+    let result = ctx.ingest_document_as(file, format).await?;
     assert(ctx, result).await?;
 
     // ingest second time
 
-    let result = ctx.ingest_document("osv/RUSTSEC-2021-0079.json").await?;
+    let result = ctx.ingest_document_as(file, format).await?;
     assert(ctx, result).await?;
 
     // done
+
+    Ok(())
+}
+
+async fn assert_common(
+    ctx: &TrustifyContext,
+    result: &IngestResult,
+    expected_vuln_id: &str,
+) -> anyhow::Result<()> {
+    let Id::Uuid(id) = result.id else {
+        bail!("must be an id")
+    };
+    let adv = ctx
+        .graph
+        .get_advisory_by_id(id, ())
+        .await?
+        .expect("must be found");
+
+    assert_eq!(adv.vulnerabilities(()).await?.len(), 1);
+
+    let all = adv.vulnerabilities(&()).await?;
+    assert_eq!(all.len(), 1);
+    assert_eq!(
+        all[0].advisory_vulnerability.vulnerability_id,
+        expected_vuln_id
+    );
+
+    let all = ctx.graph.get_vulnerabilities(()).await?;
+    assert_eq!(all.len(), 1);
+
+    let vuln = ctx
+        .graph
+        .get_vulnerability(expected_vuln_id, ())
+        .await?
+        .expect("Must be found");
+
+    assert_eq!(vuln.vulnerability.id, expected_vuln_id);
+
+    let descriptions = vuln.descriptions("en", ()).await?;
+    assert_eq!(descriptions.len(), 0);
 
     Ok(())
 }
