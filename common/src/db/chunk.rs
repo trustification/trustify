2d1ee1b70decaf11e819338a3120674985aee322
Jens Reimann <ctron@dentrassi.de>
2024-09-12 11:14:52 +0200

    refactor: clean up chunked_with, document, explain, fix

--- common/src/db/chunk.rs
+++ common/src/db/chunk.rs
@@ -27,9 +27,23 @@ where
     chunked_with(<M::Entity as EntityTrait>::Column::iter().count(), i)
 }
 
+/// Chunk up the iterator into batches suitable for ingesting into the database.
+///
+/// The idea is to create batches, which fit into the maximum number of parameters for an SQL
+/// statement, when doing `insert_many`. Currently, the limit is `u16::MAX`, which is the limit
+/// for PostgreSQL.
+///
+/// It works like this: `values` is the number of values that will be inserted per item. So the
+/// batch size is `u16::MAX / values`. As there may be additional parameters, aside from the ones
+/// used for inserting values, we lower the PostgreSQL limit by 128.
+///
+/// Also, if the number of values per entry exceeds the total maximum, we create batches of 1. This
+/// will lead to an error when executing the statement (too many parameters), but that's just the
+/// way it is anyway, chunking or not.
 pub fn chunked_with<I>(values: usize, i: I) -> IntoChunks<impl Iterator<Item = I::Item>>
 where
     I: IntoIterator,
 {
-    i.into_iter().chunks(u16::MAX as usize / values - 8)
+    i.into_iter()
+        .chunks(((u16::MAX - 128) as usize / values).max(1))
 }
