b816049decdcedd2de6a9a116886186d708a5e57
Jens Reimann <ctron@dentrassi.de>
2024-09-23 11:05:16 +0200

    feat: add an endpoint for release information

---
+++ server/src/endpoints.rs
@@ -0,0 +1,68 @@
+use actix_web::{
+    get,
+    http::header::AUTHORIZATION,
+    web::{self, ServiceConfig},
+    HttpRequest, HttpResponse,
+};
+use build_info::BuildInfo;
+use std::sync::Arc;
+use trustify_auth::authenticator::{user::UserInformation, Authenticator};
+use trustify_infrastructure::app::new_auth;
+use utoipa::OpenApi;
+
+pub fn configure(svc: &mut ServiceConfig, auth: Option<Arc<Authenticator>>) {
+    let mut scope = web::scope("/.well-known/trustify");
+
+    if let Some(auth) = auth {
+        scope = scope.app_data(web::Data::from(auth));
+    }
+
+    svc.service(scope.service(info));
+}
+
+#[derive(OpenApi)]
+#[openapi(paths(info), components(schemas(crate::endpoints::Info)), tags())]
+pub struct ApiDoc;
+
+#[derive(Clone, Debug, PartialEq, Eq, serde::Serialize, utoipa::ToSchema)]
+struct Info<'a> {
+    version: &'a str,
+    #[serde(skip_serializing_if = "Option::is_none")]
+    #[schema(value_type = serde_json::Object)]
+    build: Option<&'a BuildInfo>,
+}
+
+build_info::build_info!(fn build_info);
+
+#[utoipa::path(
+    context_path = "/.well-known/trustify",
+    responses(
+        (status = 200, description = "Get information", body = inline(Info)),
+    ),
+)]
+#[get("")]
+pub async fn info(req: HttpRequest, auth: Option<web::Data<Authenticator>>) -> HttpResponse {
+    let details = match auth {
+        // authentication is disabled, enable details
+        None => true,
+        Some(auth) => {
+            if let Some(bearer) = req
+                .headers()
+                .get(AUTHORIZATION)
+                .and_then(|auth| auth.to_str().ok())
+                .and_then(|auth| auth.strip_prefix("Bearer "))
+            {
+                // enable details if we have a valid token
+                auth.validate_token(&bearer).await.is_ok()
+            } else {
+                // no token that we can use, disable details
+                false
+            }
+        }
+    };
+
+    HttpResponse::Ok().json(Info {
+        version: env!("CARGO_PKG_VERSION"),
+        build: details.then(build_info),
+    })
+}
