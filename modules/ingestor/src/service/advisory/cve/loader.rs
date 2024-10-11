a1089243e3939e90419c13cb8ec5d476a3f47b3e
Jens Reimann <ctron@dentrassi.de>
2024-10-11 11:44:47 +0200

    chore: clean up cve project data conversion for version schemes

--- modules/ingestor/src/service/advisory/cve/loader.rs
+++ modules/ingestor/src/service/advisory/cve/loader.rs
@@ -285,13 +285,26 @@ impl<'g> CveLoader<'g> {
     }
 }
 
+/// Translate from a CVE project version type to our internal version scheme.
+///
+/// Also see: <https://github.com/CVEProject/cve-schema/blob/6af5c9c49c5b62e7b1f46756e1f3aef328848e1c/schema/CVE_Record_Format.json#L306-L318>
+///
+/// However, the reality looks quite weird. The following command can be run to get an overview of
+/// what the current state holds. Run from the `cves` directory of the repository from:
+/// <https://github.com/CVEProject/cvelistV5>
+///
+/// ```bash
+/// find -name "CVE-*.json" -exec jq '.containers.cna.affected?[]?.versions?[]?.versionType | select (. != null )' {} \; | sort -u
+/// ```
 fn try_from_cve_version_scheme(scheme: &str) -> Result<VersionScheme, ()> {
     Ok(match scheme {
-        "semver" => VersionScheme::Semver,
+        "commit" | "git" => VersionScheme::Git,
+        "custom" => VersionScheme::Generic,
         "maven" => VersionScheme::Maven,
-        "rpm" => VersionScheme::Rpm,
+        "npm" => VersionScheme::Semver,
         "python" => VersionScheme::Python,
-        "git" | "custom" => VersionScheme::Generic,
+        "rpm" => VersionScheme::Rpm,
+        "semver" => VersionScheme::Semver,
         _ => return Err(()),
     })
 }
