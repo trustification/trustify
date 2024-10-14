9eb6b584c8013e0867402e8c429dbe7d51f87638
Jens Reimann <ctron@dentrassi.de>
2024-10-14 14:23:02 +0200

    chore: remove unnamed test

--- dev/bench/data.js
+++ dev/bench/data.js
@@ -16,33 +16,6 @@ window.BENCHMARK_DATA = {
             "username": "ctron"
           },
           "distinct": true,
-          "id": "527c1230b0f209eb8e6df7e38b43a3424b2feba3",
-          "message": "ci: try ds3 ingestion benchmark",
-          "timestamp": "2024-10-14T13:20:44+02:00",
-          "tree_id": "a9e9d1a89ac38b99877e1fc5f8c1e2c001b5a669",
-          "url": "https://github.com/ctron/trustify/commit/527c1230b0f209eb8e6df7e38b43a3424b2feba3"
-        },
-        "date": 1728905192188,
-        "tool": "customSmallerIsBetter",
-        "benches": [
-          {
-            "value": 23
-          }
-        ]
-      },
-      {
-        "commit": {
-          "author": {
-            "email": "ctron@dentrassi.de",
-            "name": "Jens Reimann",
-            "username": "ctron"
-          },
-          "committer": {
-            "email": "ctron@dentrassi.de",
-            "name": "Jens Reimann",
-            "username": "ctron"
-          },
-          "distinct": true,
           "id": "23ef3e12c375264f95a2a4fe511ce4efe1c2de81",
           "message": "ci: ds3 ingestion benchmark",
           "timestamp": "2024-10-14T13:32:06+02:00",
