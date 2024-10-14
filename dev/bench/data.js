79d3b15b10d43dc6b48d2519f9ff755e2314bbd8
github-action-benchmark <github@users.noreply.github.com>
2024-10-14 11:26:32 +0000

    add Benchmark (customSmallerIsBetter) benchmark result for 527c1230b0f209eb8e6df7e38b43a3424b2feba3

---
+++ dev/bench/data.js
@@ -0,0 +1,35 @@
+window.BENCHMARK_DATA = {
+  "lastUpdate": 1728905192810,
+  "repoUrl": "https://github.com/ctron/trustify",
+  "entries": {
+    "Benchmark": [
+      {
+        "commit": {
+          "author": {
+            "email": "ctron@dentrassi.de",
+            "name": "Jens Reimann",
+            "username": "ctron"
+          },
+          "committer": {
+            "email": "ctron@dentrassi.de",
+            "name": "Jens Reimann",
+            "username": "ctron"
+          },
+          "distinct": true,
+          "id": "527c1230b0f209eb8e6df7e38b43a3424b2feba3",
+          "message": "ci: try ds3 ingestion benchmark",
+          "timestamp": "2024-10-14T13:20:44+02:00",
+          "tree_id": "a9e9d1a89ac38b99877e1fc5f8c1e2c001b5a669",
+          "url": "https://github.com/ctron/trustify/commit/527c1230b0f209eb8e6df7e38b43a3424b2feba3"
+        },
+        "date": 1728905192188,
+        "tool": "customSmallerIsBetter",
+        "benches": [
+          {
+            "value": 23
+          }
+        ]
+      }
+    ]
+  }
+}
