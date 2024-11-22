bb66dc250eb090dc17120513a4545c3adf4e981c
Jens Reimann <ctron@dentrassi.de>
2024-11-22 11:35:33 +0100

    chore: add tofu/terraform example

---
+++ docs/book/modules/admin/examples/trustify/main.tf
@@ -0,0 +1,21 @@
+terraform {
+  required_version = ">= 1.5.0"
+}
+
+variable "availability-zone" {
+  type        = string
+  default     = "eu-west-1a"
+  description = "The AWS availability zone to create RDS resources in. Must be part of the 'region'."
+}
+
+variable "environment" {
+  type        = string
+  default     = "default"
+  description = "An environment, using for tagging and creating a suffix for AWS resources"
+}
+
+variable "namespace" {
+  type        = string
+  default     = "trustify"
+  description = "The Kubernetes namespaces the resources will be created in"
+}
---
+++ docs/book/modules/admin/examples/trustify/sso.tf
@@ -0,0 +1,124 @@
+variable "sso-domain" {
+  type = string
+}
+
+variable "admin-email" {
+  type = string
+}
+
+variable "console-url" {
+  type = string
+}
+
+resource "aws_cognito_user_pool" "pool" {
+  name = "trustify-${var.environment}"
+}
+
+resource "aws_cognito_resource_server" "trustify" {
+  identifier   = "trustify"
+  name         = "trustify"
+  user_pool_id = aws_cognito_user_pool.pool.id
+
+  scope {
+    scope_description = "sbom"
+    scope_name        = "sbom"
+  }
+  scope {
+    scope_description = "advisory"
+    scope_name        = "advisory"
+  }
+  scope {
+    scope_description = "metadata"
+    scope_name        = "metadata"
+  }
+  scope {
+    scope_description = "importer"
+    scope_name        = "importer"
+  }
+}
+
+resource "aws_cognito_user_pool_domain" "main" {
+  domain       = var.sso-domain
+  user_pool_id = aws_cognito_user_pool.pool.id
+}
+
+resource "aws_cognito_user_group" "manager" {
+  user_pool_id = aws_cognito_user_pool.pool.id
+  name         = "manager"
+}
+
+resource "aws_cognito_user" "admin" {
+  user_pool_id = aws_cognito_user_pool.pool.id
+  username     = "admin"
+
+  attributes = {
+    email          = var.admin-email
+    email_verified = true
+  }
+}
+
+resource "aws_cognito_user_in_group" "admin-manager" {
+  group_name   = aws_cognito_user_group.manager.name
+  user_pool_id = aws_cognito_user_pool.pool.id
+  username     = aws_cognito_user.admin.username
+}
+
+resource "aws_cognito_user_pool_client" "cli" {
+  name         = "walker-${var.environment}"
+  user_pool_id = aws_cognito_user_pool.pool.id
+
+  supported_identity_providers = ["COGNITO"]
+
+  allowed_oauth_flows_user_pool_client = true
+
+  allowed_oauth_flows  = ["client_credentials"]
+  allowed_oauth_scopes = aws_cognito_resource_server.trustify.scope_identifiers
+
+  generate_secret = true
+}
+
+resource "kubernetes_secret" "oidc-cli" {
+  metadata {
+    name      = "oidc-cli"
+    namespace = var.namespace
+  }
+
+  data = {
+    client-id     = aws_cognito_user_pool_client.cli.id
+    client-secret = aws_cognito_user_pool_client.cli.client_secret
+  }
+
+  type = "Opaque"
+}
+
+resource "aws_cognito_user_pool_client" "frontend" {
+  name         = "frontend-${var.environment}"
+  user_pool_id = aws_cognito_user_pool.pool.id
+
+  supported_identity_providers = ["COGNITO"]
+
+  allowed_oauth_flows_user_pool_client = true
+  allowed_oauth_flows                  = ["code"]
+  allowed_oauth_scopes                 = ["email", "openid"]
+
+  callback_urls = [
+    var.console-url,
+    "${var.console-url}/",
+  ]
+  logout_urls = [
+    "${var.console-url}/notloggedin",
+  ]
+}
+
+resource "kubernetes_secret" "oidc-frontend" {
+  metadata {
+    name      = "oidc-frontend"
+    namespace = var.namespace
+  }
+
+  data = {
+    client-id = aws_cognito_user_pool_client.frontend.id
+  }
+
+  type = "Opaque"
+}
