a3638acd0442a2af28f6af9f04008b07354eb977
Jens Reimann <ctron@dentrassi.de>
2024-12-06 09:40:12 +0100

    test: added a test case for TC-1817

--- modules/fundamental/tests/sbom/spdx/corner_cases.rs
+++ modules/fundamental/tests/sbom/spdx/corner_cases.rs
@@ -1,16 +1,19 @@
 #![allow(clippy::expect_used)]
 
 use anyhow::bail;
+use bytes::BytesMut;
+use futures_util::TryStreamExt;
 use sea_orm::ConnectionTrait;
 use strum::VariantArray;
 use test_context::test_context;
 use test_log::test;
 use trustify_common::{id::Id, purl::Purl};
 use trustify_entity::relationship::Relationship;
-use trustify_module_fundamental::sbom::model::SbomPackageReference;
-use trustify_module_fundamental::sbom::service::SbomService;
-use trustify_module_ingestor::graph::purl::qualified_package::QualifiedPackageContext;
-use trustify_module_ingestor::graph::sbom::SbomContext;
+use trustify_module_fundamental::{sbom::model::SbomPackageReference, sbom::service::SbomService};
+use trustify_module_ingestor::graph::{
+    purl::qualified_package::QualifiedPackageContext, sbom::SbomContext,
+};
+use trustify_module_storage::service::StorageBackend;
 use trustify_test_context::TrustifyContext;
 
 async fn related_packages_transitively<'a, C: ConnectionTrait>(
@@ -187,6 +190,46 @@ async fn self_ref_package(ctx: &TrustifyContext) -> Result<(), anyhow::Error> {
     log::info!("Packages: {packages:#?}");
 
     assert_eq!(packages.len(), 1);
+
+    Ok(())
+}
+
+#[test_context(TrustifyContext)]
+#[test(tokio::test)]
+async fn special_char(ctx: &TrustifyContext) -> Result<(), anyhow::Error> {
+    let result = ctx.ingest_document("spdx/TC-1817-1.json").await?;
+
+    let Id::Uuid(id) = result.id else {
+        bail!("must be an id")
+    };
+
+    let service = SbomService::new(ctx.db.clone());
+    let packages = service
+        .fetch_sbom_packages(id, Default::default(), Default::default(), &ctx.db)
+        .await?;
+
+    assert_eq!(packages.total, 105);
+
+    let sbom = service
+        .fetch_sbom_summary(result.id, &ctx.db)
+        .await
+        .ok()
+        .flatten()
+        .expect("must be found");
+
+    let stream = ctx
+        .storage
+        .retrieve(
+            sbom.source_document
+                .expect("must be found")
+                .try_into()
+                .expect("must be converted"),
+        )
+        .await?
+        .expect("must be found");
+    let data: BytesMut = stream.try_collect().await?;
+
+    assert_eq!(data.len(), 124250);
 
     Ok(())
 }
