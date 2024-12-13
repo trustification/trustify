de9b8b31c67a847e2c00ca683fd0f0e8e9139175
Jens Reimann <ctron@dentrassi.de>
2024-12-13 16:30:48 +0100

    feat: allow start year and years filtering for osv

--- modules/ingestor/src/service/advisory/osv/mod.rs
+++ modules/ingestor/src/service/advisory/osv/mod.rs
@@ -3,6 +3,7 @@ mod prefix;
 pub mod loader;
 pub mod translate;
 
+use crate::service::Error;
 use osv::schema::Vulnerability;
 
 /// Load a [`Vulnerability`] from YAML, using the "classic" enum representation.
@@ -23,4 +24,13 @@ pub fn to_yaml(vuln: &Vulnerability) -> Result<String, serde_yml::Error> {
     );
 
     serde_yml::to_string(&VulnerabilityWrapped(vuln))
+}
+
+/// Parse an OSV document into a [`Vulnerability`].
+pub fn parse(buffer: &[u8]) -> Result<Vulnerability, Error> {
+    let osv: Vulnerability = serde_json::from_slice(buffer)
+        .map_err(Error::from)
+        .or_else(|_| from_yaml(buffer).map_err(Error::from))?;
+
+    Ok(osv)
 }
