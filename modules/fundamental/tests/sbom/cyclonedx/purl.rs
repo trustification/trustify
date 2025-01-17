42f992dc3cab0e3f4180028a12e03e25f8471952
Jens Reimann <ctron@dentrassi.de>
2025-01-17 11:03:24 +0100

    test: add test for aliases on ancestor

--- modules/fundamental/tests/sbom/cyclonedx/purl.rs
+++ modules/fundamental/tests/sbom/cyclonedx/purl.rs
@@ -1,5 +1,7 @@
+use itertools::Itertools;
 use test_context::test_context;
 use test_log::test;
+use trustify_module_fundamental::sbom::model::{SbomNodeReference, Which};
 use trustify_module_fundamental::{
     purl::model::summary::purl::PurlSummary, sbom::service::SbomService,
 };
@@ -10,7 +12,7 @@ fn to_string(purl: &PurlSummary) -> String {
 }
 
 fn to_strings(purls: &[PurlSummary]) -> Vec<String> {
-    purls.iter().map(to_string).collect()
+    purls.iter().map(to_string).sorted_unstable().collect()
 }
 
 /// test with multiple purls
@@ -37,11 +39,35 @@ async fn simple_ref(ctx: &TrustifyContext) -> Result<(), anyhow::Error> {
     let package = &packages.items[0];
     assert_eq!(
         to_strings(&package.purl),
-        // must find one, but only one purl. despite having two with the same value.
+        // we must find two purls here, one from the main section, the other from the evidence
         vec![
             "pkg:rpm/redhat/openssl@3.0.7-18.el9_2?arch=src",
             "pkg:rpm/redhat/openssl@3.0.7-18.el9_2?arch=src&foo=bar"
         ]
+    );
+
+    // now check component
+
+    let result = service
+        .fetch_related_packages(
+            sbom_id,
+            Default::default(),
+            Default::default(),
+            Which::Left,
+            SbomNodeReference::Package("pkg:rpm/redhat/openssl@3.0.7-18.el9_2?arch=src" /* this is actually the bom-ref value */),
+            None,
+            &ctx.db,
+        )
+        .await?;
+
+    assert_eq!(result.total, 1);
+    let relation = &result.items[0];
+    assert_eq!(to_strings(&relation.package.purl),
+       // we must find two purls here, one from the main section, the other from the evidence
+       vec![
+           "pkg:generic/openssl@3.0.7?checksum=SHA-512:1aea183b0b6650d9d5e7ba87b613bb1692c71720b0e75377b40db336b40bad780f7e8ae8dfb9f60841eeb4381f4b79c4c5043210c96e7cb51f90791b80c8285e&download_url=https://pkgs.devel.redhat.com/repo/openssl/openssl-3.0.7-hobbled.tar.gz/sha512/1aea183b0b6650d9d5e7ba87b613bb1692c71720b0e75377b40db336b40bad780f7e8ae8dfb9f60841eeb4381f4b79c4c5043210c96e7cb51f90791b80c8285e/openssl-3.0.7-hobbled.tar.gz",
+           "pkg:generic/openssl@3.0.7?checksum=SHA-512:1aea183b0b6650d9d5e7ba87b613bb1692c71720b0e75377b40db336b40bad780f7e8ae8dfb9f60841eeb4381f4b79c4c5043210c96e7cb51f90791b80c8285e&download_url=https://pkgs.devel.redhat.com/repo/openssl/openssl-3.0.7-hobbled.tar.gz/sha512/1aea183b0b6650d9d5e7ba87b613bb1692c71720b0e75377b40db336b40bad780f7e8ae8dfb9f60841eeb4381f4b79c4c5043210c96e7cb51f90791b80c8285e/openssl-3.0.7-hobbled.tar.gz&foo=bar"
+       ]
     );
 
     // done
