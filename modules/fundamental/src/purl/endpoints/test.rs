aaf1893f187f1b526b23de20102024465a869e36
Jim Crossley <jim@crossleys.org>
2025-01-21 11:49:31 -0500

    Revert exposing relationships via /api/v2/purl endpoint
    Signed-off-by: Jim Crossley <jim@crossleys.org>

--- modules/fundamental/src/purl/endpoints/test.rs
+++ modules/fundamental/src/purl/endpoints/test.rs
@@ -346,23 +346,3 @@ async fn package_with_status(ctx: &TrustifyContext) -> Result<(), anyhow::Error>
 
     Ok(())
 }
-
-#[test_context(TrustifyContext)]
-#[test(actix_web::test)]
-async fn purl_relationships(ctx: &TrustifyContext) -> Result<(), anyhow::Error> {
-    let app = caller(ctx).await?;
-    ctx.ingest_documents(["cyclonedx/openssl-3.0.7-18.el9_2.cdx_1.6.sbom.json"])
-        .await?;
-
-    let src = "pkg:rpm/redhat/openssl@3.0.7-18.el9_2?arch=src";
-    let bin = "pkg:rpm/redhat/openssl@3.0.7-18.el9_2?arch=x86_64";
-
-    let uri = format!("/api/v2/purl/{}", urlencoding::encode(bin));
-    let request = TestRequest::get().uri(&uri).to_request();
-    let response: Value = app.call_and_read_body_json(request).await;
-    log::debug!("{response:#?}");
-
-    assert_eq!(src, response["relationships"]["generated_from"][0]);
-
-    Ok(())
-}
