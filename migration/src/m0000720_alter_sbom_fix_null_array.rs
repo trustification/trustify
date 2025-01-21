8e1750dd32bc6be263f0a6e52e79d489422b0c8d
Dejan Bosanac <dbosanac@redhat.com>
2025-01-21 17:00:11 +0100

    fix: sbom license migration to update nulls

--- migration/src/m0000720_alter_sbom_fix_null_array.rs
+++ migration/src/m0000720_alter_sbom_fix_null_array.rs
@@ -6,47 +6,17 @@ pub struct Migration;
 #[async_trait::async_trait]
 impl MigrationTrait for Migration {
     async fn up(&self, manager: &SchemaManager) -> Result<(), DbErr> {
-        manager.alter_table(Self::alter_table()).await?;
+        manager
+            .get_connection()
+            .execute_unprepared(include_str!(
+                "m0000720_alter_sbom_fix_null_array/migration_up.sql"
+            ))
+            .await?;
+
         Ok(())
     }
 
     async fn down(&self, _manager: &SchemaManager) -> Result<(), DbErr> {
         Ok(())
-    }
-}
-
-impl Migration {
-    fn alter_table() -> TableAlterStatement {
-        Table::alter()
-            .table(Sbom::Table)
-            .modify_column(
-                ColumnDef::new(Sbom::DataLicenses)
-                    .array(ColumnType::Text)
-                    .not_null()
-                    .default(SimpleExpr::Custom("ARRAY[]::text[]".to_string()))
-                    .to_owned(),
-            )
-            .to_owned()
-    }
-}
-
-#[derive(DeriveIden)]
-enum Sbom {
-    Table,
-    DataLicenses,
-}
-
-#[cfg(test)]
-mod test {
-    use crate::m0000720_alter_sbom_fix_null_array::Migration;
-    use crate::PostgresQueryBuilder;
-
-    #[test]
-    fn test() {
-        let sql = Migration::alter_table().build(PostgresQueryBuilder);
-        assert_eq!(
-            sql,
-            r#"ALTER TABLE "sbom" ALTER COLUMN "data_licenses" TYPE text[], ALTER COLUMN "data_licenses" SET NOT NULL, ALTER COLUMN "data_licenses" SET DEFAULT ARRAY[]::text[]"#
-        );
     }
 }
