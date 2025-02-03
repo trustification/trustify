b299e8b716ca7715a4954088940c15cf8af01277
Jens Reimann <ctron@dentrassi.de>
2025-02-03 18:16:16 +0100

    refactor: prepare for having multiple render formats

--- modules/analysis/src/service/walk.rs
+++ modules/analysis/src/service/walk.rs
@@ -1,21 +1,29 @@
 use super::*;
 
 pub trait Visitor {
+    type Output;
+
     fn node(&mut self, node: &PackageNode);
     fn edge(&mut self, source: &PackageNode, relationship: Relationship, target: &PackageNode);
+
+    fn complete(self) -> Self::Output;
 }
 
 impl AnalysisService {
-    pub fn walk<V>(&self, sbom: &str, mut v: V) -> bool
+    /// Walk the graph of an SBOM.
+    ///
+    /// This will look up the SBOM, but not load it.
+    ///
+    /// If the SBOM was not found, then [`None`] will be returned.
+    ///
+    /// Otherwise, the visitor will be called, and it will return the output of the
+    /// [`Visitor::complete`] function.
+    pub fn walk<V>(&self, sbom: &str, mut v: V) -> Option<V::Output>
     where
         V: Visitor,
     {
         let graph = self.graph.read();
-        let graph = graph.get(sbom);
-
-        let Some(graph) = graph else {
-            return false;
-        };
+        let graph = graph.get(sbom)?;
 
         for node in graph.node_weights() {
             v.node(node);
@@ -30,6 +38,6 @@ impl AnalysisService {
             }
         }
 
-        true
+        Some(v.complete())
     }
 }
