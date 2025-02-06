c16dbde0503999b5c2bbd1e60fc0f286baad8b01
Dejan Bosanac <dbosanac@redhat.com>
2025-02-06 09:58:02 +0100

    fix: refactor version scheme matching logic

--- entity/src/version_scheme.rs
+++ entity/src/version_scheme.rs
@@ -33,3 +33,29 @@ pub enum VersionScheme {
     NuGet,
     Gem,
 }
+
+/// Translate from other ecosystems to our internal version scheme.
+///
+/// For CVE see: <https://github.com/CVEProject/cve-schema/blob/6af5c9c49c5b62e7b1f46756e1f3aef328848e1c/schema/CVE_Record_Format.json#L306-L318>
+///
+/// However, the reality looks quite weird. The following command can be run to get an overview of
+/// what the current state holds. Run from the `cves` directory of the repository from:
+/// <https://github.com/CVEProject/cvelistV5>
+///
+/// ```bash
+/// find -name "CVE-*.json" -exec jq '.containers.cna.affected?[]?.versions?[]?.versionType | select (. != null )' {} \; | sort -u
+/// ```
+impl From<&str> for VersionScheme {
+    fn from(scheme: &str) -> Self {
+        match scheme {
+            "commit" | "git" => VersionScheme::Git,
+            "custom" => VersionScheme::Generic,
+            "maven" => VersionScheme::Maven,
+            "npm" => VersionScheme::Semver,
+            "python" => VersionScheme::Python,
+            "rpm" => VersionScheme::Rpm,
+            "semver" => VersionScheme::Semver,
+            _ => VersionScheme::Generic,
+        }
+    }
+}
