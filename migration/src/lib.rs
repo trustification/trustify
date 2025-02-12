5016973d4d59657a5bcaa8a6a41d93be35ee730c
JimFuller-RedHat <jfuller@redhat.com>
2025-02-12 12:00:23 +0100

    add correct indexes for api/v2/purl?q=

--- migration/src/lib.rs
+++ migration/src/lib.rs
@@ -110,6 +110,7 @@ mod m0000900_perf_indexes;
 mod m0000910_insert_packagist_version_scheme;
 mod m0000920_perf_indexes;
 mod m0000930_alter_sbom_external_node;
+mod m0000940_perf_indexes_qualified_purl;
 
 #[cfg(feature = "ai")]
 pub mod ai;
@@ -229,6 +230,7 @@ impl MigratorTrait for Migrator {
             Box::new(m0000910_insert_packagist_version_scheme::Migration),
             Box::new(m0000920_perf_indexes::Migration),
             Box::new(m0000930_alter_sbom_external_node::Migration),
+            Box::new(m0000940_perf_indexes_qualified_purl::Migration),
         ]
     }
 }
