ed1e2a46aa4cf345a092115fb076e28eebb14c20
mrizzi <mrizzi@redhat.com>
2025-02-13 16:39:08 +0100

    TC-2165 OSV GHSA - Ecosystem Maven: add test
    Signed-off-by: mrizzi <mrizzi@redhat.com>

--- modules/fundamental/tests/sbom/details.rs
+++ modules/fundamental/tests/sbom/details.rs
@@ -68,13 +68,16 @@ async fn sbom_details_cyclonedx_osv(ctx: &TrustifyContext) -> Result<(), anyhow:
         Some("GHSA-fmj7-7gfw-64pg".to_string())
     );
 
+    let maven = ctx.ingest_document("osv/GHSA-qq9f-q439-2574.json").await?;
+    assert_eq!(maven.document_id, Some("GHSA-qq9f-q439-2574".to_string()));
+
     let sbom1 = sbom
         .fetch_sbom_details(result1.id, vec![], &ctx.db)
         .await?
         .expect("SBOM details must be found");
     log::info!("SBOM1: {sbom1:?}");
 
-    assert_eq!(10, sbom1.advisories.len());
+    assert_eq!(11, sbom1.advisories.len());
     check_advisory(
         &sbom1,
         "GHSA-45c4-8wx5-qw6w",
@@ -133,6 +136,12 @@ async fn sbom_details_cyclonedx_osv(ctx: &TrustifyContext) -> Result<(), anyhow:
         &sbom1,
         "GHSA-fmj7-7gfw-64pg",
         "CVE-2024-48915",
+        Severity::Medium,
+    );
+    check_advisory(
+        &sbom1,
+        "GHSA-qq9f-q439-2574",
+        "CVE-2024-8447",
         Severity::Medium,
     );
     Ok(())
