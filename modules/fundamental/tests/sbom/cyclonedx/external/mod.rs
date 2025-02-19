9c86fb05cb359cc80cb2aea927cf63390cfbdf0d
JimFuller-RedHat <jfuller@redhat.com>
2025-02-19 12:49:45 +0100

    expand tests

--- modules/fundamental/tests/sbom/cyclonedx/external/mod.rs
+++ modules/fundamental/tests/sbom/cyclonedx/external/mod.rs
@@ -1,8 +1,11 @@
 #![allow(clippy::expect_used)]
 #![allow(clippy::unwrap_used)]
 
+use sea_orm::{ColumnTrait, EntityTrait, QueryFilter};
+
 use test_context::test_context;
 use test_log::test;
+use trustify_entity::sbom_external_node;
 use trustify_test_context::TrustifyContext;
 
 mod rh;
@@ -14,7 +17,24 @@ async fn simple_ext_1(ctx: &TrustifyContext) -> Result<(), anyhow::Error> {
     ctx.ingest_documents(["cyclonedx/simple-ext-a.json", "cyclonedx/simple-ext-b.json"])
         .await?;
 
-    // TODO: query once that side is implemented
+    let results = sbom_external_node::Entity::find()
+        .filter(
+            sbom_external_node::Column::NodeId
+                .eq("urn:cdx:a4f16b62-fea9-42c1-8365-d72d3cef37d1/2#b"),
+        )
+        .all(&ctx.db)
+        .await?;
+
+    assert_eq!(
+        results[0].external_type,
+        sbom_external_node::ExternalType::CycloneDx
+    );
+    assert_eq!(
+        results[0].external_doc_ref,
+        "a4f16b62-fea9-42c1-8365-d72d3cef37d1".to_string()
+    );
+    assert_eq!(results[0].external_node_ref, "b".to_string());
+    assert_eq!(results[0].discriminator_value, Some("2".to_string()));
 
     Ok(())
 }
