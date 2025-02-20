10543cc5a4551c7470f1876fac66b024bedb5052
Jens Reimann <ctron@dentrassi.de>
2025-02-20 18:07:22 +0100

    chore: work towards external references in query

--- modules/analysis/src/model/graph.rs
+++ modules/analysis/src/model/graph.rs
@@ -3,25 +3,71 @@ use super::*;
 #[derive(Debug, Clone, PartialEq, Eq, ToSchema, serde::Serialize, DeepSizeOf)]
 pub enum Node {
     Package(PackageNode),
+    External(ExternalNode),
+    Unknown(BaseNode),
+}
+
+impl Deref for Node {
+    type Target = BaseNode;
+
+    fn deref(&self) -> &Self::Target {
+        match self {
+            Self::Package(package) => &package.base,
+            Self::External(external) => &external.base,
+            Self::Unknown(base) => &base,
+        }
+    }
 }
 
 #[derive(Debug, Clone, PartialEq, Eq, ToSchema, serde::Serialize, DeepSizeOf)]
-pub struct PackageNode {
+pub struct BaseNode {
     pub sbom_id: String,
     pub node_id: String,
-    pub purl: Vec<Purl>,
-    pub cpe: Vec<Cpe>,
-    pub name: String,
-    pub version: String,
     pub published: String,
+
+    pub name: String,
+
     pub document_id: String,
     pub product_name: String,
     pub product_version: String,
 }
 
+#[derive(Debug, Clone, PartialEq, Eq, ToSchema, serde::Serialize, DeepSizeOf)]
+pub struct PackageNode {
+    pub base: BaseNode,
+
+    pub purl: Vec<Purl>,
+    pub cpe: Vec<Cpe>,
+    pub version: String,
+}
+
+impl Deref for PackageNode {
+    type Target = BaseNode;
+
+    fn deref(&self) -> &Self::Target {
+        &self.base
+    }
+}
+
 impl fmt::Display for PackageNode {
     fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
         write!(f, "{:?}", self.purl)
+    }
+}
+
+#[derive(Debug, Clone, PartialEq, Eq, ToSchema, serde::Serialize, DeepSizeOf)]
+pub struct ExternalNode {
+    pub base: BaseNode,
+
+    pub external_document_reference: String,
+    pub external_node_id: String,
+}
+
+impl Deref for ExternalNode {
+    type Target = BaseNode;
+
+    fn deref(&self) -> &Self::Target {
+        &self.base
     }
 }
 
