2bd2413c6dc1798eba3982abcd3ae1593652bcc2
JimFuller-RedHat <jfuller@redhat.com>
2025-03-04 15:30:18 +0100

    chore: add analysis graph endpoint test for cdx rh product variant

--- modules/analysis/src/endpoints/tests/rh_variant.rs
+++ modules/analysis/src/endpoints/tests/rh_variant.rs
@@ -107,3 +107,53 @@ async fn resolve_rh_variant_prod_comp_cdx_external_reference(
 
     Ok(())
 }
+
+#[test_context(TrustifyContext)]
+#[test(actix_web::test)]
+async fn resolve_rh_variant_prod_comp_cdx_external_reference_curl(
+    ctx: &TrustifyContext,
+) -> Result<(), anyhow::Error> {
+    let app = caller(ctx).await?;
+
+    ctx.ingest_document("cyclonedx/rh/product_component/RHEL-8.10.0.Z.MAIN+EUS.json")
+        .await?;
+    let uri = format!(
+        "/api/v2/analysis/component/{}?descendants=10",
+        urlencoding::encode("cpe:/o:redhat:enterprise_linux:8.10::baseos")
+    );
+    let request: Request = TestRequest::get().uri(&uri).to_request();
+    let response = app.call_service(request).await;
+    assert_eq!(200, response.response().status());
+
+    ctx.ingest_document(
+        "cyclonedx/rh/product_component/RHEL-8.10.0.Z_curl@7.61.1-34.el8_10.2.json",
+    )
+    .await?;
+    let uri = format!(
+        "/api/v2/analysis/component/{}?descendants=10",
+        urlencoding::encode("cpe:/o:redhat:enterprise_linux:8.10::baseos")
+    );
+    let request: Request = TestRequest::get().uri(&uri).to_request();
+    let response: Value = app.call_and_read_body_json(request).await;
+
+    log::warn!("{:?}", response);
+
+    assert!(response.contains_subset(json!({
+      "items": [
+        {
+          "node_id": "RHEL-8.10.0.Z.MAIN+EUS",
+          "document_id": "urn:uuid:6895f8e0-2bfd-331c-97f9-97369ef1f3ee/1",
+          "name": "Red Hat Enterprise Linux 8",
+          "descendants": [
+            {
+              "node_id": "pkg:rpm/redhat/curl@7.61.1-34.el8_10.2?arch=src",
+              "name": "curl",
+              "document_id": "urn:uuid:6895f8e0-2bfd-331c-97f9-97369ef1f3ee/1",
+              "relationship": "generates",
+            }]
+        }
+      ]
+    })));
+
+    Ok(())
+}
