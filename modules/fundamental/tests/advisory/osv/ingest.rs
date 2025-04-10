7bcb9939d3387b99e7d364dd0f5285f143623df4
Jim Crossley <jim@crossleys.org>
2025-04-10 14:30:14 -0400

    feat: filter SBOM's and Advisories by labels
    Fixes #491
    
    Also added convenience translation so that either `label:foo` or
    `labels:foo` will work. See unit tests for examples.

--- modules/fundamental/tests/advisory/osv/ingest.rs
+++ modules/fundamental/tests/advisory/osv/ingest.rs
@@ -7,7 +7,7 @@ use trustify_test_context::TrustifyContext;
 #[test_context(TrustifyContext)]
 #[test(tokio::test)]
 async fn pypa_yaml(ctx: &TrustifyContext) -> anyhow::Result<()> {
-    ctx.ingest_document_as("osv/PYSEC-2024-55.yaml", Format::OSV)
+    ctx.ingest_document_as("osv/PYSEC-2024-55.yaml", Format::OSV, ("source", "test"))
         .await?;
 
     Ok(())
