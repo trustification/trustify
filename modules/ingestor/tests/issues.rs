98fe9c269ca97cbff4302cf9418b4b4c1165ff83
Jens Reimann <ctron@dentrassi.de>
2025-04-11 10:12:54 +0200

    fix: ensure that an invalid CVSS does not panic
    Closes: #1547

--- modules/ingestor/tests/issues.rs
+++ modules/ingestor/tests/issues.rs
@@ -1,3 +1,5 @@
+#![allow(clippy::expect_used)]
+
 use test_context::test_context;
 use test_log::test;
 use trustify_common::id::Id;
@@ -12,6 +14,22 @@ async fn issue_1492(ctx: &TrustifyContext) -> anyhow::Result<()> {
         .await?;
 
     assert!(matches!(result.id, Id::Uuid(_)));
+
+    Ok(())
+}
+
+#[test_context(TrustifyContext)]
+#[test(actix_web::test)]
+/// Ingested SBOM should not fail
+async fn cvss_issue_1(ctx: &TrustifyContext) -> anyhow::Result<()> {
+    let result = ctx
+        .ingest_document("csaf/issues/cvss_1/ssa-054046.json")
+        .await;
+
+    assert_eq!(
+        result.expect_err("must be an error").to_string(),
+        "unknown CVSS metric name: `E` at line 3001 column 11"
+    );
 
     Ok(())
 }
