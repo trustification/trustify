d07db669d623a5b4d2228c0d79be710bcc80756e
Jim Crossley <jim@crossleys.org>
2025-04-17 12:35:06 -0400

    feat: support nested fields within in-memory query contexts
    This mirrors the new colon-delimited query field feature for SQL
    queries, but the analysis module needs to not only query the db but
    also the in-memory graph.

--- common/src/db/query/value.rs
+++ common/src/db/query/value.rs
@@ -1,19 +1,30 @@
 use chrono::{Local, NaiveDateTime};
 use human_date_parser::{ParseResult, from_human_time};
-use std::cmp::Ordering;
+use std::{cmp::Ordering, collections::HashMap};
 use time::OffsetDateTime;
 
-pub trait Valuable: PartialOrd<String> {
-    fn like(&self, pat: &str) -> bool;
-}
-
+#[derive(Clone)]
 pub enum Value<'a> {
-    String(&'a str),
+    String(String),
     Int(i32),
     Float(f64),
     Date(&'a OffsetDateTime),
     Array(Vec<Value<'a>>),
     Custom(&'a dyn Valuable),
+}
+
+pub struct ValueContext<'a> {
+    values: HashMap<String, Value<'a>>,
+    nested: HashMap<String, HashMap<String, Value<'a>>>,
+}
+
+pub trait Context {
+    fn get(&self, key: &str) -> Option<&Value>;
+    fn values(&self) -> impl Iterator<Item = &Value>;
+}
+
+pub trait Valuable: PartialOrd<String> {
+    fn like(&self, pat: &str) -> bool;
 }
 
 impl Valuable for Value<'_> {
@@ -37,7 +48,7 @@ impl PartialEq<String> for Value<'_> {
 impl PartialOrd<String> for Value<'_> {
     fn partial_cmp(&self, rhs: &String) -> Option<Ordering> {
         match self {
-            Self::String(s) => s.partial_cmp(&rhs.as_str()),
+            Self::String(s) => s.partial_cmp(rhs),
             Self::Int(v) => match rhs.parse::<i32>() {
                 Ok(i) => v.partial_cmp(&i),
                 _ => None,
@@ -86,11 +97,87 @@ impl<'a, T: Valuable> From<&'a Vec<T>> for Value<'a> {
     }
 }
 
+impl Context for &ValueContext<'_> {
+    fn get(&self, key: &str) -> Option<&Value> {
+        self.values.get(key).or_else(|| {
+            key.split_once(':')
+                .and_then(|(k, v)| self.nested.get(k).and_then(|m| m.get(v)))
+        })
+    }
+    fn values(&self) -> impl Iterator<Item = &Value> {
+        self.values
+            .values()
+            .chain(self.nested.values().flat_map(|m| m.values()))
+    }
+}
+
+impl Default for ValueContext<'_> {
+    fn default() -> Self {
+        let values = HashMap::default();
+        let nested = HashMap::default();
+        Self { values, nested }
+    }
+}
+
+impl<'a> ValueContext<'a> {
+    pub fn put_string<S: Into<String>>(&mut self, key: S, value: S) {
+        self.values.insert(key.into(), Value::String(value.into()));
+    }
+    pub fn put_int<K: Into<String>>(&mut self, key: K, value: i32) {
+        self.values.insert(key.into(), Value::Int(value));
+    }
+    pub fn put_float<K: Into<String>>(&mut self, key: K, value: f64) {
+        self.values.insert(key.into(), Value::Float(value));
+    }
+    pub fn put_date<K: Into<String>>(&mut self, key: K, value: &'a OffsetDateTime) {
+        self.values.insert(key.into(), Value::Date(value));
+    }
+    pub fn put_array<K: Into<String>>(&mut self, key: K, value: Vec<Value<'a>>) {
+        self.values.insert(key.into(), Value::Array(value));
+    }
+    pub fn put_value<K: Into<String>>(&mut self, key: K, value: Value<'a>) {
+        self.values.insert(key.into(), value);
+    }
+
+    // Maintain a shallow nesting of values to support colon-delimited
+    // fields. When keys are re-inserted into the nested map, the
+    // existing string values are not overwritten; they are converted
+    // to array values.
+    pub fn put_nested<K, V>(&mut self, key: K, values: V)
+    where
+        K: AsRef<str>,
+        V: Iterator<Item = (String, String)>,
+    {
+        for (k, v) in values {
+            self.nested
+                .entry(key.as_ref().to_string())
+                .and_modify(|m| {
+                    m.entry(k.clone())
+                        .and_modify(|val| match val {
+                            Value::Array(arr) => {
+                                arr.push(Value::String(v.clone()));
+                            }
+                            _ => *val = Value::Array(vec![val.clone(), Value::String(v.clone())]),
+                        })
+                        .or_insert(Value::String(v.clone()));
+                })
+                .or_insert(HashMap::from([(k, Value::String(v))]));
+        }
+    }
+
+    // Convenient initialization
+    pub fn from<K: AsRef<str>, const N: usize>(arr: [(K, Value<'a>); N]) -> Self {
+        Self {
+            values: HashMap::from(arr.map(|(k, v)| (k.as_ref().to_string(), v))),
+            ..Default::default()
+        }
+    }
+}
+
 #[cfg(test)]
 pub(crate) mod tests {
     use super::super::*;
     use super::*;
-    use std::collections::HashMap;
     use test_log::test;
 
     #[test(tokio::test)]
@@ -98,8 +185,8 @@ pub(crate) mod tests {
         use time::format_description::well_known::Rfc2822;
         let now = time::OffsetDateTime::now_utc();
         let then = OffsetDateTime::parse("Sat, 12 Jun 1993 13:25:19 GMT", &Rfc2822)?;
-        let context = HashMap::from([
-            ("id", Value::String("foo")),
+        let context = ValueContext::from([
+            ("id", Value::String("foo".into())),
             ("count", Value::Int(42)),
             ("score", Value::Float(6.66)),
             ("detected", Value::Date(&then)),
@@ -127,12 +214,12 @@ pub(crate) mod tests {
     }
 
     #[test(tokio::test)]
-    async fn filter_array_values() -> Result<(), anyhow::Error> {
+    async fn filter_array_custom_values() -> Result<(), anyhow::Error> {
         use crate::purl::Purl;
 
         let purl = Purl::from_str("pkg:x/foo").unwrap();
-        let purls = vec![Value::Custom(&purl), Value::String("pkg:x/bar")];
-        let context = HashMap::from([("purl", Value::Array(purls))]);
+        let purls = vec![Value::Custom(&purl), Value::String("pkg:x/bar".into())];
+        let context = ValueContext::from([("purl", Value::Array(purls))]);
 
         assert!(q("purl=pkg:x/foo").apply(&context));
         assert!(!q("purl!=pkg:x/foo").apply(&context));
@@ -149,6 +236,37 @@ pub(crate) mod tests {
         assert!(q("pkg:x/bar").apply(&context));
         assert!(q("foo|bar").apply(&context));
         assert!(!q("bizz|buzz").apply(&context));
+
+        Ok(())
+    }
+
+    #[test(tokio::test)]
+    async fn filter_nested_values() -> Result<(), anyhow::Error> {
+        use crate::purl::Purl;
+
+        let purl = Purl::from_str("pkg:rpm/foo@1.0?k1=v1&k2=v2")?;
+        let parts = HashMap::from(
+            [("ty", "rpm"), ("name", "foo"), ("version", "1.0")]
+                .map(|(k, v)| (k.to_string(), v.to_string())),
+        );
+        let other = HashMap::from(
+            [("ty", "maven"), ("name", "bar"), ("version", "42")]
+                .map(|(k, v)| (k.to_string(), v.to_string())),
+        );
+
+        let mut context = ValueContext::from([("purl", Value::Custom(&purl))]);
+        context.put_nested("qualifiers", purl.qualifiers.clone().into_iter());
+        context.put_nested("purl", parts.into_iter());
+        context.put_nested("purl", other.into_iter()); // shouldn't overwrite
+
+        assert!(q("purl~pkg:rpm/foo").apply(&context));
+        assert!(q("qualifiers:k1=v1").apply(&context));
+        assert!(q("qualifiers:k2!=v1").apply(&context));
+        assert!(!q("qualifiers:missing!=anything").apply(&context));
+        assert!(q("purl:name=foo").apply(&context));
+        assert!(q("purl:version=1.0").apply(&context));
+        assert!(q("purl:version=42").apply(&context));
+        assert!(!q("purl:version<1").apply(&context));
 
         Ok(())
     }
