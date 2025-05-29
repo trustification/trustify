3abbe077967b2e0d697c992e52d8d91c290f0c86
Carlos Feria <2582866+carlosthe19916@users.noreply.github.com>
2025-05-29 14:28:56 +0200

    Add endpoint to fetch all labels from SBOM|Advisory
    Signed-off-by: Carlos Feria <2582866+carlosthe19916@users.noreply.github.com>
    (cherry picked from commit ca0b67e53229dec53a205fa643c61e6d276d6f2e)

--- modules/fundamental/src/sbom/model/raw_sql.rs
+++ modules/fundamental/src/sbom/model/raw_sql.rs
@@ -124,3 +124,21 @@ pub fn product_advisory_info_sql() -> String {
         CONTEXT_CPE_FILTER_SQL
     )
 }
+
+pub const SEARCH_LABELS_SQL: &str = r#"
+SELECT DISTINCT ON (kv.key, kv.value)
+    kv.key,
+    CASE
+        WHEN kv.value IS NULL OR kv.value = '' THEN NULL
+        ELSE kv.value
+    END AS value
+FROM sbom,
+    LATERAL jsonb_each_text(labels) AS kv
+WHERE
+    CASE 
+        WHEN kv.value IS NULL THEN kv.key
+        ELSE kv.key || '=' || kv.value
+    END ILIKE $1
+ORDER BY
+    kv.key, kv.value
+"#;
