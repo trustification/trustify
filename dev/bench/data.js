45b67e458d438e3fd097868cffa7ab57f9326607
github-action-benchmark <github@users.noreply.github.com>
2025-06-03 10:05:58 +0000

    add Benchmark (customSmallerIsBetter) benchmark result for ec9926aae84fa6cc51b3c5788662cb3844ce8be9

--- dev/bench/data.js
+++ dev/bench/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1748605543645,
+  "lastUpdate": 1748945158516,
   "repoUrl": "https://github.com/trustification/trustify",
   "entries": {
     "Benchmark": [
@@ -10463,6 +10463,35 @@ window.BENCHMARK_DATA = {
           "url": "https://github.com/trustification/trustify/commit/f99ca8738899adc8e1e29ba4d1e052b349fa6edf"
         },
         "date": 1748605542428,
+        "tool": "customSmallerIsBetter",
+        "benches": [
+          {
+            "name": "Ingest DS3",
+            "value": 8,
+            "unit": "s"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "ctron@dentrassi.de",
+            "name": "Jens Reimann",
+            "username": "ctron"
+          },
+          "committer": {
+            "email": "ctron@dentrassi.de",
+            "name": "Jens Reimann",
+            "username": "ctron"
+          },
+          "distinct": true,
+          "id": "ec9926aae84fa6cc51b3c5788662cb3844ce8be9",
+          "message": "ci(codecov): limit to one concurrent job\n\nWe run into SIGTERM issues during compilation. Which indicates a\nmemory limit we hit. Limiting to one job might work around this.",
+          "timestamp": "2025-06-03T09:23:25Z",
+          "tree_id": "0b0560950109f706ec852b8d5a4df1e576d2ee83",
+          "url": "https://github.com/trustification/trustify/commit/ec9926aae84fa6cc51b3c5788662cb3844ce8be9"
+        },
+        "date": 1748945157556,
         "tool": "customSmallerIsBetter",
         "benches": [
           {
