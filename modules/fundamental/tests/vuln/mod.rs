12d9ab389cbd233ca506ea490611502c2cba5273
Jens Reimann <ctron@dentrassi.de>
2025-07-09 16:11:36 +0200

    fix(fundamental): change response of analyse purl endpoint
    Skip returning SBOM specific content as the endpoint should return
    results for this PURL, not related SBOMs.
    
    Remove duplicates in response. Due to the way the SQL query was
    structured, it returned duplicate entries.
    
    This will now return all advisories contributing to this state. It will
    also skip not_affected and recommended, in addition to fixed.
    
    Maps will also have a stable, sorted order of keys.

--- modules/fundamental/tests/vuln/mod.rs
+++ modules/fundamental/tests/vuln/mod.rs
@@ -1,8 +1,11 @@
+#![allow(clippy::expect_used)]
+
 use itertools::Itertools;
+use serde_json::json;
 use test_context::test_context;
 use test_log::test;
 use trustify_module_fundamental::vulnerability::service::VulnerabilityService;
-use trustify_test_context::{Dataset, TrustifyContext};
+use trustify_test_context::{Dataset, TrustifyContext, subset::ContainsSubset};
 
 #[test_context(TrustifyContext)]
 #[test(tokio::test)]
@@ -17,9 +20,15 @@ async fn issue_1840(ctx: &TrustifyContext) -> Result<(), anyhow::Error> {
 
     println!("{:#?}", result);
 
+    // check number of PURLs
+
     assert_eq!(result.len(), 1);
 
+    // get expected purl
+
     let entry = &result["pkg:rpm/redhat/gnutls@3.7.6-23.el9?arch=aarch64"];
+
+    // test for vulnerability IDs
 
     let ids = entry
         .iter()
@@ -27,16 +36,33 @@ async fn issue_1840(ctx: &TrustifyContext) -> Result<(), anyhow::Error> {
         .sorted()
         .collect::<Vec<_>>();
 
-    // TODO: find out why we return four times the same
-    assert_eq!(
-        ids,
-        vec![
-            "CVE-2024-28834",
-            "CVE-2024-28834",
-            "CVE-2024-28834",
-            "CVE-2024-28834"
-        ]
+    assert_eq!(ids, vec!["CVE-2024-28834"]);
+
+    // now check advisories
+
+    let vuln_entry = entry
+        .iter()
+        .find(|e| e.head.identifier == "CVE-2024-28834")
+        .expect("must find entry");
+
+    assert_eq!(vuln_entry.status.len(), 1);
+
+    let status_entry = &vuln_entry.status["affected"];
+
+    assert_eq!(status_entry.len(), 1);
+    let json = serde_json::to_value(status_entry).expect("must serialize");
+    assert!(
+        json.contains_subset(json!([{
+            "document_id": "CVE-2024-28834",
+            "identifier": "https://www.redhat.com/#CVE-2024-28834",
+            "modified": "2025-01-07T01:43:37Z",
+            "published": "2024-03-21T00:00:00Z",
+            "title": "gnutls: vulnerable to Minerva side-channel information leak",
+        }])),
+        "doesn't match: {json:#?}"
     );
+
+    // done
 
     Ok(())
 }
